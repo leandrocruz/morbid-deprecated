18c7e7581a7c0c1b4cce4230931a9909cb6d47f6
Leandro Cruz <leandro.saad@gmail.com>
2019-03-08 15:20:11 -0300

    Using a 'local.conf' configuration as config.file

--- backend/build/build-docker-image.sh
+++ backend/build/build-docker-image.sh
@@ -12,7 +12,7 @@ unzip -q ../target/universal/package.zip -d tmp
 cp Dockerfile tmp
 cat << EOF > tmp/run
 #!/usr/bin/env bash
-/opt/morbid/service/bin/run
+/opt/morbid/service/bin/run -Dconfig.file=/opt/morbid/service/shared/conf/local.conf
 EOF
 
 chmod +x tmp/run
